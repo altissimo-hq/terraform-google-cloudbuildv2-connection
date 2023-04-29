0f1cd887aa1648963fb099c386b8a8724cb7abd0
Lukas Karlsson <karlsson@altissimo.io>
2023-04-29 13:14:26 -0400

    Renamed resource to have underscores.

--- secrets.tf
+++ secrets.tf
@@ -1,5 +1,5 @@
 # Create the Secret Manager Secret, if create_secret is true
-resource "google_secret_manager_secret" "github-oauth-token" {
+resource "google_secret_manager_secret" "github_auth_token" {
   count     = var.create_secret ? 1 : 0
   project   = local.secret_project
   secret_id = var.oauth_token_secret
@@ -23,17 +23,17 @@ data "google_project" "project" {
 }
 
 # Grant the Cloud Build Service Account access to the Secret Manager Secret
-resource "google_secret_manager_secret_iam_member" "clouduild-github-oauth-token" {
+resource "google_secret_manager_secret_iam_member" "clouduild_github_auth_token" {
   count     = var.create_secret ? 1 : 0
-  project   = google_secret_manager_secret.github-oauth-token[0].project
-  secret_id = google_secret_manager_secret.github-oauth-token[0].secret_id
+  project   = google_secret_manager_secret.github_auth_token[0].project
+  secret_id = google_secret_manager_secret.github_auth_token[0].secret_id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"
 }
 
 # Retrieve the value of the Secret Manager Secret Version
-data "google_secret_manager_secret_version" "github-oauth-token" {
+data "google_secret_manager_secret_version" "github_auth_token" {
   count   = var.create_secret ? 1 : 0
   project = local.secret_project
-  secret  = google_secret_manager_secret.github-oauth-token[0].secret_id
+  secret  = google_secret_manager_secret.github_auth_token[0].secret_id
 }
